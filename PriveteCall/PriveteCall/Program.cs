1a7c65ca3c2a491aa31408c9731767d4823dd858
Akira Kawamata <autumn@piedey.co.jp>
2021-10-29 14:03:07 +0900

    add PrivateCall

---
+++ PriveteCall/PriveteCall/Program.cs
@@ -0,0 +1,12 @@
+﻿var s = new MyClass();
+var mytype = s.GetType();
+var subMethod = mytype.GetMethod("sub", System.Reflection.BindingFlags.NonPublic|System.Reflection.BindingFlags.Instance);
+subMethod?.Invoke(s,null);
+
+class MyClass
+{
+    private void sub()
+    {
+        Console.WriteLine("Hello I'm Private");
+    }
+}
