613edd0968e9bdeedee18bed8dd32c4d7bfe0692
Akira Kawamata <autumn@piedey.co.jp>
2021-10-29 14:20:19 +0900

    add  InitAllStaticClass

---
+++ InitAllStaticClass/InitAllStaticClass/Program.cs
@@ -0,0 +1,19 @@
+﻿foreach (var item in System.Reflection.Assembly.GetExecutingAssembly().GetTypes())
+{
+    if (item.IsInterface) continue;
+    Console.WriteLine($"Found static class {item.FullName}");
+    System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(item.TypeHandle);
+}
+
+static class A
+{
+    static A() => Console.WriteLine("A initialized");
+}
+static class B
+{
+    static B() => Console.WriteLine("B initialized");
+}
+static class C
+{
+    static C() => Console.WriteLine("C initialized");
+}
