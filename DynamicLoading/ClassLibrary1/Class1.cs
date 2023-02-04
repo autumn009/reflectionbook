8adb34b9b9cb83803750ce5b2b5460eb223e8636
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 14:31:06 +0900

    add DynamicLoading

---
+++ DynamicLoading/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Sub()
+        {
+            Console.WriteLine("Hello, I'm ClassLibrary1.Class1.Sub!");
+        }
+    }
+}
---
+++ DynamicLoading/DynamicLoading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+checkSub();
+Console.WriteLine("Loading");
+var assembly = Assembly.LoadFrom(@"..\..\..\..\ClassLibrary1\bin\Debug\net7.0\ClassLibrary1.dll");
+checkSub();
+
+void checkSub()
+{
+    foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+    {
+        var type = item.GetType("ClassLibrary1.Class1");
+        if( type != null )
+        {
+            var method = type.GetMethod("Sub", BindingFlags.Static| BindingFlags.Public);
+            if( method != null)
+            {
+                Console.WriteLine("Found");
+                method?.Invoke(null, null);
+                return;
+            }
+        }
+    }
+    Console.WriteLine("Not Found");
+}
