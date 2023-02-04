2ba02bd18ac566d039608d5624af59fe6f553fa7
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 19:43:16 +0900

    add IdentifyByType

---
+++ IdentifyByType/IdentifyByType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+{
+    var type = item.GetInterface("CommonTypeX");
+    if (type != null)
+    {
+        var x = Activator.CreateInstance(item) as CommonTypeX;
+        if (x != null) x.Sub();
+    }
+}
+
+interface CommonTypeX
+{
+    void Sub();
+}
+
+class A : CommonTypeX
+{
+    public void Sub() => Console.WriteLine("I'm class A!");
+}
+class B : CommonTypeX
+{
+    public void Sub() => Console.WriteLine("I'm class B!");
+}
+class C
+{
+    public void Sub() => Console.WriteLine("I'm class C!");
+}
