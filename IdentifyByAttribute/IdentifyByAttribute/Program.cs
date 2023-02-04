6bfb6faa5c9f5f5c120f545043ab2ed1f626e88f
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 19:50:59 +0900

    add IdentifyByAttribute

---
+++ IdentifyByAttribute/IdentifyByAttribute/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+{
+    var type = item.GetCustomAttribute<MyTargetAttribute>();
+    if (type != null) Console.WriteLine($"Found Type {item.FullName}");
+}
+
+[MyTarget]
+class A { }
+
+[MyTarget]
+class B { }
+
+class C { }
+
+class MyTargetAttribute : Attribute { }
