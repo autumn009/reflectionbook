8bf8d914e5733fb4feee1b4cd8eeafa83c9567f0
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 10:39:47 +0900

    add Count1

---
+++ Count1/Count1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+Console.WriteLine($"I have {Assembly.GetExecutingAssembly().GetTypes().Count()} types. They are ...");
+foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+{
+    Console.WriteLine(item.FullName);
+}
