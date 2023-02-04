8d895267c7effaab34f04a592cf18fb44771666d
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 19:02:54 +0900

    add DynamicType

---
+++ DynamicType/DynamicType/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+Console.WriteLine("by new");
+var a = new Nemo();
+a.Sub();
+
+Console.WriteLine("by reflection");
+var type = Type.GetType("Nemo");
+if (type != null)
+{
+    dynamic? instance = Activator.CreateInstance(type);
+    instance?.Sub();
+}
+
+class Nemo
+{
+    public void Sub()
+    {
+        Console.WriteLine("in Nemo.Sub");
+    }
+}
+
+
