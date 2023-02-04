fb92f5d1fdf1669b06a48d8520e128e0e85bb1f6
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 15:29:27 +0900

    fix eol

---
+++ DynamicLoad2/DynamicLoad2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+var resolver = new PathAssemblyResolver(new string[] { "ExampleAssembly.dll", typeof(object).Assembly.Location });
+
+
+
+
+var mlc = new MetadataLoadContext(resolver);
+
+using (mlc)
+{
+    // Load assembly into MetadataLoadContext.
+    Assembly assembly = mlc.LoadFromAssemblyPath("ExampleAssembly.dll");
+    AssemblyName name = assembly.GetName();
+
+    // Print assembly attribute information.
+    Console.WriteLine($"{name.Name} has following attributes: ");
+
+    foreach (CustomAttributeData attr in assembly.GetCustomAttributesData())
+    {
+        try
+        {
+            Console.WriteLine(attr.AttributeType);
+        }
+        catch (FileNotFoundException ex)
+        {
+            // We are missing the required dependency assembly.
+            Console.WriteLine($"Error while getting attribute type: {ex.Message}");
+        }
+    }
+}
+
