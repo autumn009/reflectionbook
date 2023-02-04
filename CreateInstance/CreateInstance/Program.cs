169f671aa539b68a7d80ed609ede8e88233da299
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 18:47:30 +0900

    add CreateInstance

---
+++ CreateInstance/CreateInstance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+Console.WriteLine("by new");
+var a = new Nemo();
+a.Sub();
+
+Console.WriteLine("by reflection");
+var type = Type.GetType("Nemo");
+if (type != null)
+{
+    object? instance = System.Activator.CreateInstance(type);
+    if( instance != null )
+    {
+        var method = type.GetMethod("Sub",BindingFlags.Instance|BindingFlags.Public);
+        method?.Invoke(instance, null);
+    }
+}
+
+class Nemo
+{
+    public void Sub()
+    {
+        Console.WriteLine("in Nemo.Sub");
+    }
+}
+
+
