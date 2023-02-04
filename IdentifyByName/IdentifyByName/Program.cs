d961ddbcdf373c00362282b461c5aa376ae121d8
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 19:03:36 +0900

    add IdentifyByName:

---
+++ IdentifyByName/IdentifyByName/Program.cs
@@ -0,0 +1,25 @@
+﻿int typeCount = 0;
+int memberCount = 0;
+
+foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
+{
+    foreach (var type in assem.GetTypes())
+    {
+        if (type.FullName != null && type.FullName.StartsWith("System.Reflection."))
+        {
+            if (type.IsPublic)
+            {
+                Console.WriteLine(type.FullName);
+                typeCount++;
+
+                foreach (var member in type.GetMembers())
+                {
+                    Console.WriteLine($"  {member.Name} ({member.MemberType})");
+                    memberCount++;
+                }
+            }
+        }
+    }
+}
+Console.WriteLine($"type count={typeCount}");
+Console.WriteLine($"member count={memberCount}");
