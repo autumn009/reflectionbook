27833cd93417930ea2a968e5b12cf815ce2c9dda
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 11:42:37 +0900

    add Count2

---
+++ Count2/Count2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine($"found {AppDomain.CurrentDomain.GetAssemblies().Sum(c => c.GetTypes().Count())} types");
+foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+{
+    var name = item.FullName ?? "(NO NAME),";
+    int index = name.IndexOf(',');
+    Console.WriteLine($"{item.GetTypes().Count(),4} types in {name.Substring(0,index)}");
+}
