dd75020fb19965785400565cc4ac823b41b97c92
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 20:56:06 +0900

    temp save

---
+++ DetectBoxing/DetectBoxing/Program.cs
@@ -0,0 +1,17 @@
+﻿object a = 1;
+object b = "2";
+sub(nameof(a), a);
+sub(nameof(b), b);
+
+void sub(string label, object x)
+{
+    Console.WriteLine($"{label} is {IsBoxed(x)}");
+}
+
+bool IsBoxed<T>(T value)
+{
+    return
+        (typeof(T).IsInterface || typeof(T) == typeof(object)) &&
+        value != null &&
+        value.GetType().IsValueType;
+}
