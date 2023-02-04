7c37f6ea2ba96762e280b7aa1705d8f89e8939a6
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 18:13:44 +0900

    fix more

--- DynamicMetaLoading/DynamicMetaLoading/Program.cs
+++ DynamicMetaLoading/DynamicMetaLoading/Program.cs
@@ -1,21 +1,18 @@
 ﻿using System.Reflection;
-using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 
 const string path = @"..\..\..\..\ClassLibrary1\bin\Debug\net7.0\ClassLibrary1.dll";
 
-string[] runtimeAssemblies = Directory.GetFiles(RuntimeEnvironment.GetRuntimeDirectory(), "*.dll");
-var paths = new List<string>(runtimeAssemblies);
-paths.Add(Path.GetFileName(path));
+var paths = Directory.GetFiles(RuntimeEnvironment.GetRuntimeDirectory(), "*.dll").Append(path);
 var resolver = new PathAssemblyResolver(paths);
 
 using (var mlc = new MetadataLoadContext(resolver))
 {
-    var assembly =  mlc.LoadFromAssemblyPath(path);
+    var assembly = mlc.LoadFromAssemblyPath(path);
     var stream = assembly.GetManifestResourceStream("ClassLibrary1.TextFile1.txt");
     var bytes = new byte[256];
     stream?.Read(bytes, 0, bytes.Length);
-    var s = Encoding.UTF8.GetString(bytes);
+    var s = Encoding.UTF8.GetString(bytes).Substring(1);
     Console.WriteLine(s);
 }
