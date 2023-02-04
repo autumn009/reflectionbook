eef8007daffeb0ddb1fd26c70ce87145683069ec
Akira Kawamata <autumn@piedey.co.jp>
2023-02-04 20:12:28 +0900

    add ExcludeByAttribute

---
+++ ExcludeByAttribute/ExcludeByAttribute/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+{
+    if (item.IsSubclassOf(typeof(Base)))
+    {
+        var type = item.GetCustomAttribute<ExcludeAttribute>();
+        if (type == null) Console.WriteLine($"Found Type {item.FullName}");
+    }
+}
+
+class Base { }
+
+class A: Base { }
+
+class B: Base { }
+
+[Exclude]
+class C: Base { }
+
+class ExcludeAttribute : Attribute { }
