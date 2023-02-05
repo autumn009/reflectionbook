c46097dc9c6367b029ee4e2f52858cfa49ef2b9f
Akira Kawamata <autumn@piedey.co.jp>
2023-02-05 11:53:17 +0900

    add CollectTypedObjects

---
+++ CollectTypedObjects/CollectTypedObjects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+var persons = CollectTypedObjects<Person>(Assembly.GetExecutingAssembly());
+foreach (var person in persons) Console.WriteLine(person.GetName());
+
+TargetType[] CollectTypedObjects<TargetType>(Assembly targetAssembly)
+{
+    List<TargetType> list = new List<TargetType>();
+    foreach (var t in targetAssembly.GetTypes())
+    {
+        if (t.IsAbstract) continue;
+        if (t.IsSubclassOf(typeof(TargetType)))
+        {
+            if (t.GetCustomAttributes(typeof(ExcludeAttribute), false).Length == 0)
+            {
+                object? obj = Activator.CreateInstance(t);
+                if (obj != null) list.Add((TargetType)obj);
+            }
+        }
+    }
+    return list.ToArray();
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class ExcludeAttribute : Attribute { }
+
+abstract class Person
+{
+    public abstract string GetName();
+}
+
+class Taro : Person
+{
+    public override string GetName() => "たろう";
+}
+
+class Jiro : Person
+{
+    public override string GetName() => "じろう";
+}
+
+class Saburo : Person
+{
+    public override string GetName() => "さぶろう";
+}
