1f490d2dfd10b237974d694f1d9201d7857950af
Akira Kawamata <autumn@piedey.co.jp>
2023-02-05 10:03:40 +0900

    add DetectBoxing

--- DetectBoxing/DetectBoxing/Program.cs
+++ DetectBoxing/DetectBoxing/Program.cs
@@ -1,17 +1,36 @@
 ﻿object a = 1;
-object b = "2";
+object b = "Text";
+ISample c = new X();
 sub(nameof(a), a);
 sub(nameof(b), b);
+sub(nameof(c), c);
 
-void sub(string label, object x)
+void sub<T>(string label, T x)
 {
-    Console.WriteLine($"{label} is {IsBoxed(x)}");
+    var s = IsBoxed(x) ? "" : "not ";
+    Console.WriteLine($"{label} is {s}boxed");
 }
 
 bool IsBoxed<T>(T value)
 {
+#if DUMP
+    Console.WriteLine($"typeof(T).IsInterface={typeof(T).IsInterface}");
+    Console.WriteLine($"typeof(T) == typeof(object)={typeof(T) == typeof(object)}");
+    Console.WriteLine($"value != null={value != null}");
+    Console.WriteLine($"value.GetType().IsValueType={value.GetType().IsValueType}");
+#endif
     return
         (typeof(T).IsInterface || typeof(T) == typeof(object)) &&
         value != null &&
         value.GetType().IsValueType;
+}
+
+interface ISample
+{
+    void DummySub();
+}
+
+struct X : ISample
+{
+    public void DummySub() => Console.WriteLine("I'm X.Sub!");
 }
