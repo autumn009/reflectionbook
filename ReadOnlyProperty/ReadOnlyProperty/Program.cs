c34e9d5c268b0ced36dd37922a5d472d240157e0
Akira Kawamata <autumn@piedey.co.jp>
2023-02-05 10:47:27 +0900

    ReadOnlyProperty

---
+++ ReadOnlyProperty/ReadOnlyProperty/Program.cs
@@ -0,0 +1,21 @@
+﻿var x = new X();
+x.DumpProps();
+Console.WriteLine("Rewriting to 999...");
+foreach (var prop in x.GetType().GetProperties())
+{
+    if (prop.CanWrite) prop.SetValue(x, 999);
+}
+x.DumpProps();
+
+class X
+{
+    public int MyProperty1 { get; set; } = 1;
+    public int MyProperty2 { get; set; } = 2;
+    public int MyProperty3 { get; } = 3;
+    public void DumpProps()
+    {
+        Console.WriteLine($"MyProperty1={MyProperty1}");
+        Console.WriteLine($"MyProperty2={MyProperty2}");
+        Console.WriteLine($"MyProperty3={MyProperty3}");
+    }
+}
