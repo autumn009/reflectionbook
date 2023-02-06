b5538b348eb3043fee0d7dd83f923d3a086fdea7
Akira Kawamata <autumn@piedey.co.jp>
2023-02-06 11:06:56 +0900

    add Type2

---
+++ Type2/Type2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 型から直接取得する
+var a = typeof(MySampleClass);
+Console.WriteLine(a.FullName);
+
+// インスタンスから取得する
+var instance = new MySampleClass();
+var b = instance.GetType();
+Console.WriteLine(b.FullName);
+
+// 名前から取得する
+var c = Type.GetType("MySampleClass");
+Console.WriteLine(c?.FullName);
+
+class MySampleClass { }
