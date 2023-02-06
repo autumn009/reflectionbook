1c878b217bdeab92e9e28976bb5ef8b7df355f2f
Akira Kawamata <autumn@piedey.co.jp>
2023-02-06 11:32:31 +0900

    add IsSubclassOf

--- Inherit/Inherit/Program.cs
+++ Inherit/Inherit/Program.cs
@@ -1,4 +1,5 @@
 ﻿var type = typeof(object).GetType();
+Console.WriteLine($"type.IsSubclassOf(typeof(Type))={type.IsSubclassOf(typeof(Type))}");
 for (; ; )
 {
     Console.WriteLine($"type.FullName={type.FullName}");
